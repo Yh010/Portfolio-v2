bda819e02e8d440e79758fc48588bbcb20cdc196
Yash Hegde <yash4success@gmail.com>
2024-06-24 20:18:53 +0530

    contact me section

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import Experience from "./experience/experience";
+import ContactMe from "./contactMe/ContactMe";
 
 function App() {
   return (
@@ -13,13 +14,16 @@ function App() {
       achievements
        */}
       <div className="columns-1 md:columns-3">
-        <div className="h-screen invisible"></div>
+        <div className="h-screen ">
+          <div className="mt-4">
+            <ContactMe />
+          </div>
+        </div>
         <div className="pt-12 h-screen overflow-auto no-scrollbar">
           <div className="flex-col ">
             <div className="flex justify-between">
               <div className="font-sans font-semi-bold text-lg">Yash Hegde</div>
               <div className="flex space-x-4 font-thin">
-                <div className="hover:text-blue-600">Contact Me</div>
                 <a
                   className="hover:text-blue-600 after:content-['_↗'] ..."
                   href="https://drive.google.com/file/d/1mLEgEQslrW-JjZPKWW7ieLjqcCu2eF7B/view?usp=sharing"
